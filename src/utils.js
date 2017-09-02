a7920bd1bf0bd5ce853e06a2ce56668b50aea15b
chencheng <sorrycc@gmail.com>
2017-09-02 16:57:19 +0800

    moved

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-
-export isPlainObject from 'is-plain-object';
-export const isArray = Array.isArray.bind(Array);
-export const isFunction = o => typeof o === 'function';
-export const returnSelf = m => m;
-export const noop = () => {};
